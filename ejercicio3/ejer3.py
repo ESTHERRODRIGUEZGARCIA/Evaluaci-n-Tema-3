c3a2bd584fcc408df7d4a653f8367844ca6c44ea
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-28 16:24:29 +0200

    comienzo ejer POO

---
+++ ejercicio3/ejer3.py
@@ -0,0 +1,5 @@
+#naves de Star Wars
+#nombre, largo, tripulación y cantidad de pasajeros,
+class Naves(object):
+    def __init__(self, nombre:str, largo:float, tripul:str, pasajeros:int):
+        self.n = 0
