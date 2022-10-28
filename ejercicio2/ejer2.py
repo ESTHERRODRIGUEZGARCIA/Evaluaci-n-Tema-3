ace81d61177aca338fa5b5ed503cf6b4a960f969
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-28 16:53:22 +0200

    defino matriz

---
+++ ejercicio2/ejer2.py
@@ -0,0 +1,14 @@
+#matrix 3x3
+
+
+print ("Ingrese los valores: \n |a00 a01 a02|\n |a10 a11 a12| \n |a20 a21 a22|")
+# defino los valores del la matriz
+a00 = float(input('Ingrese el valor a00: '))
+a01 = float(input('Ingrese el valor a01: '))
+a02 = float(input('Ingrese el valor a02: '))
+a10 = float(input('Ingrese el valor a10:'))
+a11 = float(input('Ingrese el valor a11: '))
+a12 = float(input('Ingrese el valor a12: '))
+a20 = float(input('Ingrese el valor a20: '))
+a21 = float(input('Ingrese el valor a21: '))
+a22 = float(input('Ingrese el valor a22: '))
