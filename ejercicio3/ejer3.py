435546958685cf0db71958910653eadbe75720a3
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-28 16:28:31 +0200

    dos listas de largo y nombre

--- ejercicio3/ejer3.py
+++ ejercicio3/ejer3.py
@@ -2,4 +2,16 @@
 #nombre, largo, tripulación y cantidad de pasajeros,
 class Naves(object):
     def __init__(self, nombre:str, largo:float, tripul:str, pasajeros:int):
-        self.n = 0
+        self.nombre = nombre
+        self.largo = largo
+        self.tripul = tripul
+        self.pasajeros = pasajeros
+
+    def lista(self):
+        listan=[]
+        listan.append(self.nombre)
+        print(listan)
+        listal = []
+        listal.append(self.largo)
+        print(listal)
+        
