aaefa78a768764f43119eada2f2131317069f945
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-28 15:29:12 +0200

    ejer 4

--- ejercicio1/ejer1.py
+++
@@ -1,9 +0,0 @@
-class datoPolinomio(object):
-    def __init__(self, valor, termino):
-        self.valor = valor
-        self.termino = termino
-
-class Polinomio(object):
-    def __init__(self):
-        self.termino_mayor = None
-        self.grado = -1
