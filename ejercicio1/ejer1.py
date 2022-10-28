1f851449b35366ea2fae2c127d09e1ee61471a7b
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-28 15:55:14 +0200

    preparación movimientos

--- ejercicio1/ejer1.py
+++ ejercicio1/ejer1.py
@@ -1,4 +1,12 @@
 #Torre de Hanoi
 
-def movimiento():
-    
+def movimiento(tf, td):
+    global torres
+    texto = str(torres[tf][-1])+ '=' + tf + '=>' + td 
+    torres[td].append(torres[tf][-1])
+
+    del torres[tf][-1]
+    movimiento.append(texto)
+
+
+
