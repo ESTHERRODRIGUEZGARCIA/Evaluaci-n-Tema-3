59c8419663e1ba758497a6bf45368b62c67a4b41
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-28 16:55:14 +0200

    multiplicación matriz hecho

--- ejercicio2/ejer2.py
+++ ejercicio2/ejer2.py
@@ -12,3 +12,15 @@ a12 = float(input('Ingrese el valor a12: '))
 a20 = float(input('Ingrese el valor a20: '))
 a21 = float(input('Ingrese el valor a21: '))
 a22 = float(input('Ingrese el valor a22: '))
+
+#hago los cálculos para calcular el determinante por Sarrus
+total1=a00*a11*a22 + a10*a21*a02 +a20*a01*a12
+total2=total1+(a02*a11*a20)*-1 + (a12*a21*a00)*-1 + (a22*a01*a10)*-1
+
+
+
+print("El determinante de la matriz ")
+print("|",a00, a01, a02,"|")
+print("|",a10, a11, a12,"|")
+print("|",a20, a21, a22,"|")
+print("es: ",total2)
