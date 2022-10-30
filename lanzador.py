8d75e9462e9ed979b951f79a44fc2edbe61c8d3b
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-30 13:25:41 +0100

    lanzador del 4

--- lanzador.py
+++ lanzador.py
@@ -1,1 +1,19 @@
 #ejer 4 print(Polinomio.resta)
+from ejercicio4 import *
+import sympy
+
+
+def main4():
+
+    print('Ejercicio 4')
+    p1 = input("Primer Polinomio: ")
+    p2 = input("Segundo Polinomio: ")
+    print("\n")
+
+    #Definimos los simbolos
+    sympy.init_printing()
+    x,y = sympy.symbols('x,y')
+
+    #Luego almacenamos en varibles los dos polinomios procesados por la funcion Poly de sympy
+    Poly1 = sympy.Poly(p1)
+    Poly2 = sympy.Poly(p2)
