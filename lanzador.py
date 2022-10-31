011d99fe4abaa7bb01c42eea0239ee9c1edf2eb2
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-31 11:30:59 +0100

    hago bien ejer4

--- lanzador.py
+++ lanzador.py
@@ -1,52 +1,22 @@
 #ejer 4 print(Polinomio.resta)
-from ejercicio4 import *
-import sympy
-from ejercicio4.Clases4 import *
-from ejercicio4.funciones import *
-from ejercicio1 import *
-
-def main4():
-    p1 = Polinomio()
-    agregar_termino(p1, 2, 8)
-    p2 = Polinomio()
-    agregar_termino(p2, 2, 8)
-    print(mostrar(p1))
-    print(mostrar(p2))
-    print(mostrar(resta(p1, p2)))
-    print(mostrar(dividir(p1, p2)))
-    print(determinar_existe(p1, 2))
-    print(determinar_existe(p2, 2))
-    eliminar_termino(p1, 2)
-    eliminar_termino(p2, 2)
-    print(mostrar(p1))
-    print(mostrar(p2))
+from ejercicios import Polinomio, agregar_termino, dividir, mostrar, eliminar_termino, determinar_existe, resta
 
 
 
+p1 = Polinomio()
+agregar_termino(p1, 2, 1)
+p2 = Polinomio()
+agregar_termino(p2, 1, 2)
+nuevo_pol = dividir(p2, p1)
+print(mostrar(p1))
+print(mostrar(p2))
+print(mostrar(resta(p1, p2)))
+print(mostrar(dividir(p1, p2)))
+print(determinar_existe(p1, 2))
+print(determinar_existe(p2, 2))
+eliminar_termino(p1, 2)
+eliminar_termino(p2, 2)
+print(mostrar(p1))
+print(mostrar(p2))
 
 
-
-
-
-
-    print('Ejercicio 4')
-    p1 = input("Primer Polinomio: ")
-    p2 = input("Segundo Polinomio: ")
-    print("\n")
-
-    #Definimos los simbolos
-    sympy.init_printing()
-    x,y = sympy.symbols('x,y')
-
-    #Luego almacenamos en varibles los dos polinomios procesados por la funcion Poly de sympy
-    Poly1 = sympy.Poly(p1)
-    Poly2 = sympy.Poly(p2)
-
-    print(Polinomio.resta)
-    print(Polinomio.division)
-    print(Polinomio.eliminar_termino)
-    print(Polinomio.determinar_existe)
-
-
-def elegir():
-    ejercicio1.
