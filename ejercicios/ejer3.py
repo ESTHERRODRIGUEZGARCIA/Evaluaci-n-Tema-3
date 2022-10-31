d854843b775a8781f3ba235bcadcb94e2ca70de8
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-31 09:12:17 +0100

    tenía fallos; se abre bien csv

--- ejercicios/ejer3.py
+++ ejercicios/ejer3.py
@@ -2,7 +2,7 @@
 #nombre, largo, tripulación y cantidad de pasajeros,
 import csv
 
-with open('naves_sw', r) as file:
+with open('naves_sw.csv', 'r') as file:
     reader = csv.DictReader(file, delimiter=';')
     for row in reader:
         print(row)
