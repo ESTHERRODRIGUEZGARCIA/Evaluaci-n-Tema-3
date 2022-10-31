e3637c36188a67f736e5713d792b3e8e6bead0f6
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-31 11:33:08 +0100

    errores

--- lanzador.py
+++ lanzador.py
@@ -1,5 +1,5 @@
 #ejer 4 print(Polinomio.resta)
-from ejercicios import Polinomio, agregar_termino, dividir, mostrar, eliminar_termino, determinar_existe, resta
+from ejercicios.ejer4 import Polinomio, agregar_termino, division, mostrar, eliminar_termino, determinar_existe, resta
 
 
 
@@ -7,11 +7,11 @@ p1 = Polinomio()
 agregar_termino(p1, 2, 1)
 p2 = Polinomio()
 agregar_termino(p2, 1, 2)
-nuevo_pol = dividir(p2, p1)
+nuevo_pol = division(p2, p1)
 print(mostrar(p1))
 print(mostrar(p2))
 print(mostrar(resta(p1, p2)))
-print(mostrar(dividir(p1, p2)))
+print(mostrar(division(p1, p2)))
 print(determinar_existe(p1, 2))
 print(determinar_existe(p2, 2))
 eliminar_termino(p1, 2)
