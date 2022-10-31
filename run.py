6bfc3a30a18cf004b3a6c98e4cf1cf73ee4aefe4
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-31 11:31:27 +0100

    nuevo 4

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-from ejercicio3 import ejer3.ordenar_naves
+
+import re
+from ejercicios import ejer1, ejer2, ejer3
+
+
 def ejecutar():
-    ejer3.ordenar_naves
+    eleccion = int(input('¿Qué ejercicio 1-5 desea ejecutar? \nPara finalizar introduzca 6. '))
+    if eleccion == 1:
+        ejer1()
+    elif eleccion == 2:
+        ejer2()
+    elif eleccion == 3:
+        ejer3()
+    elif eleccion == 4:
+        pass
+    elif eleccion == 5:
+        pass
+    elif eleccion == 6:
+        print("Finished. ")
+
