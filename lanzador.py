dd1a2a644a085ba524aec0b3d8ab1bda30d1ff97
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-31 12:55:42 +0100

    aclaro

--- lanzador.py
+++ lanzador.py
@@ -7,20 +7,25 @@ def main4():
     p2 = Polinomio()
     agregar_termino(p2, 1, 2)
 
-
+    print("Mostrar ambos polinomios:")
     print(mostrar(p1))
     print(mostrar(p2))
 
+    print("Mostrar su resta:")
     print(mostrar(resta(p1, p2)))
 
+    print("Mostrar su división:")
     print(mostrar(division(p1, p2)))
 
+    print("DEterminar si existe el término en cada polinomio: ")
     print(determinar_existe(p1, 2))
     print(determinar_existe(p2, 2))
 
+    print("Eliminar el término: ")
     eliminar_termino(p1, 2)
     eliminar_termino(p2, 2)
 
+    print("Mostrar de nuevo cada polinomio:")
     print(mostrar(p1))
     print(mostrar(p2))
 main4()
