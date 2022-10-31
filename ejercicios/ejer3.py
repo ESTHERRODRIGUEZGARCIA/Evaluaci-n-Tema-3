c661052fbd7b57d4a40d6fef7967e91fac52a9a7
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-31 09:24:58 +0100

    fallos

--- ejercicios/ejer3.py
+++ ejercicios/ejer3.py
@@ -5,13 +5,12 @@ import csv
 with open('naves_sw.csv', 'r') as file:
     print("Se muestra la lista de naves para este ejercicio:")
     reader = csv.DictReader(file, delimiter=';')
-    naves_starwars = list(reader)
     for row in reader:
         print(row)
 
 def lista_nombres():
     nombres = []
-    for row in naves_starwars:
+    for row in reader:
         nombres.append(row['nombre'])
     nombres.sort()
     print("La lista de naves ordenadas por su nombre es: ", {nombres})
