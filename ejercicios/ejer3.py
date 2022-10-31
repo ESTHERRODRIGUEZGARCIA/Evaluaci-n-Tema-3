ca242ced33bb439a421f01bf1f07c705155975b2
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-31 09:16:40 +0100

    hago puntos

--- ejercicios/ejer3.py
+++ ejercicios/ejer3.py
@@ -3,6 +3,9 @@
 import csv
 
 with open('naves_sw.csv', 'r') as file:
+    print("Se muestra la lista de naves para este ejercicio:")
     reader = csv.DictReader(file, delimiter=';')
     for row in reader:
         print(row)
+        #naves_starwars = list(reader)
+
